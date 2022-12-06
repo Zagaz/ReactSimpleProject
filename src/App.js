7a2a237119b065857c880f8ba759ca1b8e51d236
“André <andre_ranulfo@hotmail.com>
2022-12-06 18:54:47 -0300

    #60 Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,94 +1,145 @@
-import { useState , useRef , useEffect } from 'react'
-import './App.css'
-import Contact from "./Components/Contact"
-import { v4 as chave } from 'uuid'
-//import ContactList from './Components/ContactList'
-
+import { useState, useRef, useEffect } from "react";
+import "./App.css";
+import Contact from "./Components/Contact";
+import { v4 as chave } from "uuid";
 
 function App() {
   //useState
-  const [contact , setContact] = useState({id : "" , name: "" , telephone: ""  })
-  const [contactList , setContactList] = useState([])
+  const [contact, setContact]=useState({ id: "", name: "", telephone: "" });
+  const [contactList, setContactList]=useState([]);
   //useRef
-  const inputName = useRef()
-  const inputTelephone = useRef()
+  const inputName=useRef();
+  const inputTelephone=useRef();
 
   //Methods
-  function defineName(event){
-    setContact({...contact, name : event.target.value})
+  function defineName(event) {
+    setContact({ ...contact, name: event.target.value });
   }
 
-  function defineTelephone(event){
-    setContact({...contact, telephone : event.target.value})
+  function defineTelephone(event) {
+    setContact({ ...contact, telephone: event.target.value });
   }
 
-  function addContact(){
+  function addContact() {
     //Validation
-    if(contact.name === "" || contact.telephone === "") return
+    if (contact.name===""||contact.telephone==="") return;
     //Check if it already has this entry.
-    let duplicity = contactList.find((ct)=> ct.name===contact.name && ct.telephone===contact.telephone)
-    if (typeof duplicity !== "undefined") {
-      alert ("⚠️This contact is already on the list!")
-      inputTelephone.current.focus()
-      return
+    let duplicity=contactList.find(
+      (ct) => ct.name===contact.name&&ct.telephone===contact.telephone
+    );
+    if (typeof duplicity!=="undefined") {
+      alert("⚠️This contact is already on the list!");
+      inputTelephone.current.focus();
+      return;
     }
-    
+
     //Add new contact
-    setContactList([...contactList, {...contact, id: chave()}])
+    setContactList([...contactList, { ...contact, id: chave() }]);
 
-    setContact({name: "" , telephone:""})
-    inputName.current.focus()
+    setContact({ name: "", telephone: "" });
+    inputName.current.focus();
   }
-
 
   // State persistence
 
-  useEffect(()=>{
-    if(localStorage.getItem('my_contacts') !== null){
-      setContactList(JSON.parse(localStorage.getItem('my_contacts')))
+  useEffect(() => {
+    if (localStorage.getItem("my_contacts")!==null) {
+      setContactList(JSON.parse(localStorage.getItem("my_contacts")));
     }
-  },[])
+  }, []);
 
-  useEffect(()=>{
-    localStorage.setItem('my_contacts' , JSON.stringify(contactList))
-  },[contactList])
+  useEffect(() => {
+    localStorage.setItem("my_contacts", JSON.stringify(contactList));
+  }, [contactList]);
 
-  function clearStorage(){
-   setContactList([])
+  function clearStorage() {
+    setContactList([]);
   }
 
   //Remove item from list
 
   function removeContact(id) {
-    let temp = contactList.filter(ct => ct.id !== id )
+    let temp=contactList.filter((ct) => ct.id!==id);
 
-    setContactList(temp)
-    
+    setContactList(temp);
   }
   return (
     <>
-      <h1 className='text-center'>My Awesome React Contact List</h1>
-      <hr />
-      <div>
-        <label >Name:</label> <br />
-          <input type = "text" ref={inputName} onChange = {defineName} value = {contact.name} />
-      </div>
-      <div>
-      <label>Telephone:</label> <br />
-          <input type = "text" ref ={inputTelephone} onChange = {defineTelephone}  value = {contact.telephone} />
-      </div>
-      <div>
-          <button onClick={addContact} > Add contact </button>
-          <button onClick={clearStorage} > Clear </button>
+      <div className="container-fluid title">
+        <div className="row">
+          <div className="col text-center">
+            <h4 className="text-center">MY AWESOME REACT CONTACT LIST</h4>
+          </div>
         </div>
-        
-        <hr />
-       {/* Contact list  */}
-          {contactList.map(ct =>{
-            return <Contact name={ct.name} telephone ={ct.telephone} key = {ct.id} id = {ct.id} remove ={removeContact}/>
-          })}
-       
-  
+      </div>
+
+      <div className="container-fluid theform">
+        <div className="row">
+          <div className="col p-3">
+            <div className="row justify-content-center">
+              <div className="col-10 col-sm-80 col-md-6 col-lg-4">
+                <div className="mb-3">
+                  <label className="form-label">Name</label> <br />
+                  <input
+                    className="form-control"
+                    type="text"
+                    ref={inputName}
+                    onChange={defineName}
+                    value={contact.name}
+                  />
+                </div>
+
+                <div>
+                  <label className="form-label">Telephone</label> <br />
+                  <input
+                    className="form-control"
+                    type="text"
+                    ref={inputTelephone}
+                    onChange={defineTelephone}
+                    value={contact.telephone}
+                  />
+                </div>
+                <div className="row mt-3 ">
+                  <div className="col text-start">
+                    <button
+                      className="btn btn-outline-warning"
+                      onClick={clearStorage}
+                    >
+                      {" "}
+                      Clear{" "}
+                    </button>
+                  </div>
+                  <div className="col text-end">
+                    <button
+                      className="btn btn-outline-info"
+                      onClick={addContact}
+                    >
+                      {" "}
+                      Add{" "}
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div></div>
+          </div>
+        </div>
+      </div>
+
+      <hr />
+      {/* Contact list  */}
+      {contactList.map((ct) => {
+        return (
+          <Contact
+            name={ct.name}
+            telephone={ct.telephone}
+            key={ct.id}
+            id={ct.id}
+            remove={removeContact}
+          />
+        );
+      })}
     </>
   );
 }
